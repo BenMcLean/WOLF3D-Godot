31b82813e7ba44eef54cb36b2ed983de5cb65b20
Benjamin McLean <mclean.ben@gmail.com>
2019-08-15 19:59:55 -0500

    Update PaletteFileReader.cs

--- Graphics/PaletteFileReader.cs
+++ Graphics/PaletteFileReader.cs
@@ -13,22 +13,17 @@ namespace WOLF3D.Graphics
             {
                 if (!input.ReadLine().Equals("JASC-PAL") || !input.ReadLine().Equals("0100"))
                     throw new InvalidDataException("Palette \"" + file + "\" is an incorrectly formatted JASC palette.");
-
                 if (!int.TryParse(input.ReadLine(), out int numColors)
                  || numColors != COLORS)
                     throw new InvalidDataException("Palette \"" + file + "\" does not contain exactly " + COLORS + " colors.");
-
                 for (int x = 0; x < numColors; x++)
                 {
-                    string line = input.ReadLine();
-                    string[] tokens = line?.Split(' ');
-
-                    if (tokens == null || tokens.Length != 3)
+                    string[] tokens = input.ReadLine()?.Split(' ');
+                    if (tokens == null || tokens.Length != 3
+                        || !byte.TryParse(tokens[0], out byte r)
+                        || !byte.TryParse(tokens[1], out byte g)
+                        || !byte.TryParse(tokens[2], out byte b))
                         throw new InvalidDataException("Palette \"" + file + "\" is an incorrectly formatted JASC palette.");
-
-                    byte.TryParse(tokens[0], out byte r);
-                    byte.TryParse(tokens[1], out byte g);
-                    byte.TryParse(tokens[2], out byte b);
                     result[x] = (r << 24) + (g << 16) + (b << 8) + (x == 255 ? 0 : 255);
                 }
             }
