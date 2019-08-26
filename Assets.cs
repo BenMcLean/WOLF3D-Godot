83579fffa28d5e9d8eecc0069f2f706332bdff43
Benjamin McLean <mclean.ben@gmail.com>
2019-08-25 19:16:24 -0500

    Converted all graphics to Godot objects

---
+++ Assets.cs
@@ -0,0 +1,43 @@
+﻿using Godot;
+using WOLF3DSim;
+
+namespace WOLF3D
+{
+    /// <summary>
+    /// Assets takes the bytes extracted from VSwap and creates the corresponding Godot objects for them to be used throughout the game.
+    /// </summary>
+    public class Assets
+    {
+        public VSwap VSwap { get; set; }
+        public ImageTexture[] Graphics { get; set; }
+
+        public Assets(VSwap vswap)
+        {
+            Load(vswap);
+        }
+
+        public Assets Load(VSwap vswap)
+        {
+            VSwap = vswap;
+            Graphics = new ImageTexture[VSwap.SoundPage];
+            for (uint i = 0; i < Graphics.Length; i++)
+                if (VSwap.Pages[i] != null)
+                {
+                    Godot.Image image = new Image(); ;
+                    image.CreateFromData(64, 64, false, Image.Format.Rgba8, VSwap.Pages[i]);
+                    Graphics[i] = new ImageTexture();
+                    Graphics[i].CreateFromImage(image, (int)Texture.FlagsEnum.ConvertToLinear);
+                }
+            return this;
+        }
+
+        public static readonly SpatialMaterial WallMaterial = new SpatialMaterial()
+        {
+            FlagsUnshaded = true,
+            FlagsDoNotReceiveShadows = true,
+            FlagsDisableAmbientLight = true,
+            ParamsSpecularMode = SpatialMaterial.SpecularMode.Disabled,
+            ParamsCullMode = SpatialMaterial.CullMode.Disabled
+        };
+    }
+}
--- Game.cs
+++ Game.cs
@@ -1,63 +1,41 @@
 using Godot;
+using WOLF3D;
 using WOLF3DSim;
 
 public class Game : Spatial
 {
-    public static VSwap vswap = new VSwap();
+    public static Assets Assets;
 
     /// Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         DownloadShareware.Main(new string[] { "" });
+        Assets = new Assets(new VSwap()
+            .SetPalette(@"Wolf3DSim\Palettes\Wolf3D.pal")
+            .Read(@"WOLF3D\VSWAP.WL1")
+        );
 
-        vswap.SetPalette(@"Wolf3DSim\Palettes\Wolf3D.pal");
-        vswap.Read(@"WOLF3D\VSWAP.WL1");
         //GameMaps maps = new GameMaps().Read(@"WOLF3D\MAPHEAD.WL1", @"WOLF3D\GAMEMAPS.WL1");
 
-        Godot.Image imageWall = new Image(); ;
-        imageWall.CreateFromData(64, 64, false, Image.Format.Rgba8, vswap.Pages[0]);
-        ImageTexture itWall = new ImageTexture();
-        itWall.CreateFromImage(imageWall, (int)Texture.FlagsEnum.ConvertToLinear);
-
-        AudioStreamSample audioStreamSample = new AudioStreamSample()
+        Sprite3D sprite0 = new Sprite3D
         {
-            Data = VSwap.ConcatArrays(
-                vswap.Pages[vswap.SoundPage],
-                vswap.Pages[vswap.SoundPage + 1]
-            ),
-            Format = AudioStreamSample.FormatEnum.Format8Bits,
-            MixRate = 7000,
-            Stereo = false
+            Name = "Sprite0",
+            Texture = Assets.Graphics[0],
+            Scale = new Vector3(1, 1, 1),
+            MaterialOverride = Assets.WallMaterial,
+            Transform = new Transform(Basis.Identity, new Vector3(0, 0, 1))
         };
+        AddChild(sprite0);
 
-        AudioStreamPlayer audioStreamPlayer = new AudioStreamPlayer()
+        Sprite3D sprite1 = new Sprite3D
         {
-            Stream = audioStreamSample,
-            VolumeDb = 0.01f
+            Name = "Sprite1",
+            Texture = Assets.Graphics[2],
+            Scale = new Vector3(1, 1, 1),
+            MaterialOverride = Assets.WallMaterial,
+            Transform = new Transform(Basis.Identity, new Vector3(1, 0, 0))
         };
-
-        AddChild(audioStreamPlayer);
-
-        audioStreamPlayer.Play();
-
-        SpatialMaterial spatialMaterial = new SpatialMaterial()
-        {
-            FlagsUnshaded = true,
-            FlagsDoNotReceiveShadows = true,
-            FlagsDisableAmbientLight = true,
-            ParamsSpecularMode = SpatialMaterial.SpecularMode.Disabled,
-            ParamsCullMode = SpatialMaterial.CullMode.Disabled
-        };
-
-        Sprite3D sprite3D = new Sprite3D
-        {
-            Name = "Sprite3",
-            Texture = itWall,
-            Scale = new Vector3(5, 5, 5),
-            MaterialOverride = spatialMaterial
-        };
-
-        AddChild(sprite3D);
+        AddChild(sprite1);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -65,4 +43,26 @@ public class Game : Spatial
     //  {
     //      
     //  }
+
+    public Game PlayASound()
+    {
+        AudioStreamSample audioStreamSample = new AudioStreamSample()
+        {
+            Data = VSwap.ConcatArrays(
+                Assets.VSwap.Pages[Assets.VSwap.SoundPage],
+                Assets.VSwap.Pages[Assets.VSwap.SoundPage + 1]
+            ),
+            Format = AudioStreamSample.FormatEnum.Format8Bits,
+            MixRate = 7000,
+            Stereo = false
+        };
+        AudioStreamPlayer audioStreamPlayer = new AudioStreamPlayer()
+        {
+            Stream = audioStreamSample,
+            VolumeDb = 0.01f
+        };
+        AddChild(audioStreamPlayer);
+        audioStreamPlayer.Play();
+        return this;
+    }
 }
