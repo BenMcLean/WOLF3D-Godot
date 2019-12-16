0e249a3697570fe8c86a9731098f5d48d565c919
Benjamin McLean <mclean.ben@gmail.com>
2019-12-16 13:37:28 -0600

    Update Direction8.cs

--- WOLF3DGame/Direction8.cs
+++ WOLF3DGame/Direction8.cs
@@ -1,9 +1,6 @@
 ﻿using Godot;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WOLF3DGame
 {
@@ -13,7 +10,7 @@ namespace WOLF3DGame
 
         public static readonly Direction8 SOUTH = new Direction8()
         {
-            Uint = 0,
+            Value = 0,
             ShortName = "S",
             Name = "South",
             Vector2 = Vector2.Down,
@@ -21,7 +18,7 @@ namespace WOLF3DGame
         };
         public static readonly Direction8 SOUTHWEST = new Direction8()
         {
-            Uint = 1,
+            Value = 1,
             ShortName = "SW",
             Name = "Southwest",
             Vector2 = new Vector2(-1, 1).Normalized(),
@@ -29,7 +26,7 @@ namespace WOLF3DGame
         };
         public static readonly Direction8 WEST = new Direction8()
         {
-            Uint = 2,
+            Value = 2,
             ShortName = "W",
             Name = "West",
             Vector2 = Vector2.Left,
@@ -37,7 +34,7 @@ namespace WOLF3DGame
         };
         public static readonly Direction8 NORTHWEST = new Direction8()
         {
-            Uint = 3,
+            Value = 3,
             ShortName = "NW",
             Name = "Northwest",
             Vector2 = new Vector2(1, 1).Normalized(),
@@ -45,7 +42,7 @@ namespace WOLF3DGame
         };
         public static readonly Direction8 NORTH = new Direction8()
         {
-            Uint = 4,
+            Value = 4,
             ShortName = "N",
             Name = "North",
             Vector2 = Vector2.Up,
@@ -53,7 +50,7 @@ namespace WOLF3DGame
         };
         public static readonly Direction8 NORTHEAST = new Direction8()
         {
-            Uint = 5,
+            Value = 5,
             ShortName = "NE",
             Name = "Northeast",
             Vector2 = new Vector2(1, -1).Normalized(),
@@ -61,7 +58,7 @@ namespace WOLF3DGame
         };
         public static readonly Direction8 EAST = new Direction8()
         {
-            Uint = 6,
+            Value = 6,
             ShortName = "E",
             Name = "East",
             Vector2 = Vector2.Right,
@@ -69,7 +66,7 @@ namespace WOLF3DGame
         };
         public static readonly Direction8 SOUTHEAST = new Direction8()
         {
-            Uint = 7,
+            Value = 7,
             ShortName = "SE",
             Name = "Southeast",
             Vector2 = new Vector2(-1, -1).Normalized(),
@@ -77,34 +74,46 @@ namespace WOLF3DGame
         };
         public static readonly Direction8[] Values = new Direction8[] { SOUTH, SOUTHWEST, WEST, NORTHWEST, NORTH, NORTHEAST, EAST, SOUTHEAST };
 
-        public uint Uint { get; private set; }
+        public uint Value { get; private set; }
         public string Name { get; private set; }
         public string ShortName { get; private set; }
         public Vector2 Vector2 { get; private set; }
         public Vector3 Vector3 { get; private set; }
 
-        public Direction8 Clock()
-        {
-            return add(1);
-        }
+        public static implicit operator ulong(Direction8 d) => d.Value;
+        public static implicit operator long(Direction8 d) => d.Value;
+        public static implicit operator uint(Direction8 d) => d.Value;
+        public static implicit operator int(Direction8 d) => (int)d.Value;
+        public static implicit operator ushort(Direction8 d) => (ushort)d.Value;
+        public static implicit operator short(Direction8 d) => (short)d.Value;
+        public static implicit operator byte(Direction8 d) => (byte)d.Value;
+        public static implicit operator string(Direction8 d) => d.Name;
+        public static implicit operator Vector2(Direction8 d) => d.Vector2;
+        public static implicit operator Vector3(Direction8 d) => d.Vector3;
 
-        public Direction8 Counter()
-        {
-            return add(-1);
-        }
-
-        public Direction8 Clock90()
-        {
-            return add(2);
-        }
-
-        public Direction8 Counter90()
-        {
-            return add(-2);
-        }
-
-        private Direction8 add(int @int) => Values[Modulus((((int)Uint) + @int), Values.Length)];
-
+        public Direction8 Clock => Add(1);
+        public Direction8 Counter => Add(-1);
+        public Direction8 Clock90 => Add(2);
+        public Direction8 Counter90 => Add(-2);
+        public Direction8 Clock135 => Add(3);
+        public Direction8 Counter135 => Add(-3);
+        public Direction8 Opposite => Add(4);
+        private Direction8 Add(int @int) => Values[Modulus((((int)Value) + @int), Values.Length)];
         public static int Modulus(int lhs, int rhs) => (lhs % rhs + rhs) % rhs;
+
+        public static Direction8 From(string @string)
+            => uint.TryParse(@string, out uint result) && result < Values.Length ?
+                Values[result]
+                : (from v in Values
+                   where string.Equals(v.ShortName, @string, StringComparison.CurrentCultureIgnoreCase)
+                   || string.Equals(v.Name, @string, StringComparison.CurrentCultureIgnoreCase)
+                   select v).FirstOrDefault();
+
+        public static Direction8 From(Vector3 vector3) => From(new Vector2(vector3.x, vector3.z));
+
+        public static Direction8 From(Vector2 vector2)
+        {
+            return null; // TODO: FIX THIS!
+        }
     }
 }
