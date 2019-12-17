e46393f4a651b3c45bb5a0f16d9ac4a695079d12
Benjamin McLean <mclean.ben@gmail.com>
2019-12-17 09:25:46 -0600

    Added first attempt at directional Actor

--- WOLF3DGame/Actor.cs
+++ WOLF3DGame/Actor.cs
@@ -1,17 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Godot;
+using WOLF3DGame.Model;
 
 namespace WOLF3DGame
 {
     public class Actor : Billboard
     {
+        public Direction8 Direction { get; set; } = Direction8.SOUTH;
+        public string ActorName { get; set; } = "Guard";
+        public string Animation { get; set; } = "Standing";
+        public uint Frame { get; set; } = 0;
+        private uint LastFrame { get; set; } = 0;
+
         public override void _Process(float delta)
         {
             base._Process(delta);
-
+            if (MeshInstance.Visible && Game.Assets.Animations.TryGetValue(ActorName + "/" + Animation, out uint[][] frame))
+            {
+                uint newFrame = frame[Frame][Direction8.Modulus(
+                    Direction8.Angle(
+                        new Vector2(
+                        GetViewport().GetCamera().GlobalTransform.origin.x,
+                        GetViewport().GetCamera().GlobalTransform.origin.z
+                        ).AngleToPoint(new Vector2(
+                            GlobalTransform.origin.x,
+                            GlobalTransform.origin.z
+                            ))
+                    ) + Direction,
+                    frame[Frame].Length
+                    )];
+                if (newFrame != LastFrame)
+                {
+                    MeshInstance.MaterialOverride = Game.Assets.VSwapMaterials[newFrame];
+                    LastFrame = newFrame;
+                }
+            }
         }
     }
 }
