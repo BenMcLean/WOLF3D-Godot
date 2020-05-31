f25779ae89723d74a998e05b116e9eab570f4bfe
bmclean <mclean.ben@gmail.com>
2020-05-30 22:03:30 -0500

    Added method to detect proximity to billboards (based on square hit box)

--- WOLF3DGame/Action/Billboard.cs
+++ WOLF3DGame/Action/Billboard.cs
@@ -103,5 +103,11 @@ namespace WOLF3D.WOLF3DGame.Action
                     });
             return billboards.ToArray();
         }
+
+        public bool IsClose(Vector3 vector3) => IsClose(vector3.x, vector3.z);
+        public bool IsClose(Vector2 vector2) => IsClose(vector2.x, vector2.y);
+        public bool IsClose(float x, float y) =>
+            Mathf.Abs(GlobalTransform.origin.x - x) < Assets.HalfWallWidth &&
+            Mathf.Abs(GlobalTransform.origin.z - y) < Assets.HalfWallWidth;
     }
 }
