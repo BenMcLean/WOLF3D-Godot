5236d59b42b7e379567cc089a11170a3996764cf
bmclean <mclean.ben@gmail.com>
2020-06-27 08:34:32 -0500

    Update Actor.cs

--- WOLF3DGame/Action/Actor.cs
+++ WOLF3DGame/Action/Actor.cs
@@ -1,11 +1,11 @@
 ﻿using Godot;
+using System.Xml.Linq;
 
 namespace WOLF3D.WOLF3DGame.Action
 {
     public class Actor : Billboard
     {
         public Actor() : base() => Name = "Actor";
-        public Direction8 Direction { get; set; } = Direction8.SOUTH;
         public string Animation { get; set; } = "Standing";
         public ushort Frame { get; set; } = 0;
 
@@ -25,5 +25,61 @@ namespace WOLF3D.WOLF3DGame.Action
                 && newFrame != Page)
                 Page = newFrame;
         }
+
+        #region objstruct
+        //typedef struct objstruct
+        //{
+        //    activetype active;
+        /*
+        typedef enum {
+            ac_badobject = -1,
+            ac_no,
+            ac_yes,
+            ac_allways
+        }
+        activetype;
+        */
+        //    int ticcount;
+        public ushort TicCount;
+        //    classtype obclass;
+        public string ObjClass;
+        //    statetype* state;
+        public XElement State;
+        //    byte flags;                //    FL_SHOOTABLE, etc
+        //#define FL_SHOOTABLE	1
+        public bool Shootable = false;
+        //#define FL_BONUS		2
+        //#define FL_NEVERMARK	4
+        public bool NeverMark = false;
+        //#define FL_VISABLE		8
+        //#define FL_ATTACKMODE	16
+        public bool AttackMode = false;
+        //#define FL_FIRSTATTACK	32
+        public bool FirstAttack = false;
+        //#define FL_AMBUSH		64
+        public bool Ambush = false;
+        //#define FL_NONMARK		128
+        public bool NoMark = false;
+        //    long distance;            // if negative, wait for that door to open
+        //    dirtype dir;
+        public Direction8 Direction { get; set; } = Direction8.SOUTH;
+        //    fixed x, y;
+        //    unsigned tilex, tiley;
+        //    byte areanumber;
+        //    int viewx;
+        //    unsigned viewheight;
+        //    fixed transx, transy;        // in global coord
+
+        //    int angle;
+        //    int hitpoints;
+        public ushort HitPoints = 0;
+        //    long speed;
+        public uint Speed = 0;
+
+        //    int temp1, temp2, temp3;
+        //    struct objstruct    *next,*prev;
+        //}
+        //objtype;
+        #endregion objstruct
     }
 }
