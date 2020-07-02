567304475d03f03d43b41816d6c15799ca318bc5
bmclean <mclean.ben@gmail.com>
2020-07-02 12:42:29 -0500

    Update TriangularMatrix.cs

--- WOLF3DGame/Action/TriangularMatrix.cs
+++ WOLF3DGame/Action/TriangularMatrix.cs
@@ -4,36 +4,36 @@
     {
         private T[][] Data;
 
-        public TriangularMatrix(uint length)
-        {
-            Length = length;
-            Clear();
-        }
+        public TriangularMatrix(uint length) => Length = length;
 
         public uint Length
         {
             get => (uint)(Data?.Length ?? 0);
-            set => Data = new T[value][];
+            set
+            {
+                Data = new T[value][];
+                Clear();
+            }
         }
 
         public TriangularMatrix<T> Clear()
         {
-            for (uint i = 1; i < Length; i++)
-                Data[i] = new T[i - 1];
+            for (uint i = 0; i < Length; i++)
+                Data[i] = new T[i + 1];
             return this;
         }
 
         public T this[uint x, uint y]
         {
             get => x < y ?
-                Data[y][x]
-                : Data[x][y];
+                Data[y - 1][x]
+                : Data[x - 1][y];
             set
             {
                 if (x < y)
-                    Data[y][x] = value;
+                    Data[y - 1][x] = value;
                 else
-                    Data[x][y] = value;
+                    Data[x - 1][y] = value;
             }
         }
     }
