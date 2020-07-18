e3eab3c0efc5e1a61695304714ea1a2cef492d24
bmclean <mclean.ben@gmail.com>
2020-07-18 13:37:55 -0500

    Trying 2D element over camera

--- WOLF3DGame/FadeCamera.cs
+++ WOLF3DGame/FadeCamera.cs
@@ -4,25 +4,36 @@ namespace WOLF3D.WOLF3DGame
 {
     public class FadeCamera : ARVRCamera
     {
-        public FadeCamera() => Cube = new MeshInstance()
-        {
-            Mesh = new CubeMesh()
-            {
-                Size = new Vector3(0.2f, 0.2f, 0.2f),
-                Material = new SpatialMaterial()
-                {
-                    AlbedoColor = Color.Color8(0, 0, 255, 64),
-                    FlagsUnshaded = true,
-                    FlagsDoNotReceiveShadows = true,
-                    FlagsDisableAmbientLight = true,
-                    FlagsTransparent = true,
-                    ParamsCullMode = SpatialMaterial.CullMode.Disabled,
-                    ParamsSpecularMode = SpatialMaterial.SpecularMode.Disabled,
-                    RenderPriority = int.MaxValue,
-                },
-            },
 
-        };
+        public FadeCamera()
+        {
+            AddChild(new ColorRect()
+            {
+                Color = Color.Color8(0, 0, 255, 128),
+                RectSize = new Vector2(float.MaxValue, float.MaxValue),
+            });
+        }
+        /*
+         * => Cube = new MeshInstance()
+    {
+        Mesh = new CubeMesh()
+        {
+            Size = new Vector3(0.2f, 0.2f, 0.2f),
+            Material = new SpatialMaterial()
+            {
+                AlbedoColor = Color.Color8(0, 0, 255, 64),
+                FlagsUnshaded = true,
+                FlagsDoNotReceiveShadows = true,
+                FlagsDisableAmbientLight = true,
+                FlagsTransparent = true,
+                ParamsCullMode = SpatialMaterial.CullMode.Disabled,
+                ParamsSpecularMode = SpatialMaterial.SpecularMode.Disabled,
+                RenderPriority = int.MaxValue,
+            },
+        },
+
+    };
+        */
 
         public MeshInstance Cube
         {
