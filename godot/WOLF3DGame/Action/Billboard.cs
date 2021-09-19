fe303a322228434d6f4bda942ce88ff6b4750d7d
Benjamin McLean <mclean.ben@gmail.com>
2021-09-18 19:39:54 -0500

    serialize billboard

--- godot/WOLF3DGame/Action/Billboard.cs
+++ godot/WOLF3DGame/Action/Billboard.cs
@@ -7,48 +7,12 @@ using WOLF3DModel;
 
 namespace WOLF3D.WOLF3DGame.Action
 {
-	public class Billboard : Target3D
+	public class Billboard : Target3D, ISavable
 	{
-		public static readonly BoxShape BillboardShape = new BoxShape()
-		{
-			Extents = new Vector3(Assets.HalfWallWidth, Assets.HalfWallHeight, Assets.PixelWidth / 2f),
-		};
-
+		#region Data
 		public XElement XML { get; set; }
 		public CollisionShape CollisionShape { get; private set; }
-
-		public Billboard()
-		{
-			Name = "Billboard";
-			AddChild(CollisionShape = new CollisionShape()
-			{
-				Shape = BillboardShape,
-				Transform = new Transform(Basis.Identity, new Vector3(0f, Assets.HalfWallHeight, -Assets.PixelWidth)),
-			});
-			CollisionShape.AddChild(MeshInstance = new MeshInstance()
-			{
-				Mesh = Assets.WallMesh,
-				Transform = new Transform(Basis.Identity, new Vector3(0f, 0f, Assets.PixelWidth)),
-				Visible = false,
-			});
-			Size = new Vector2(Assets.WallWidth, Assets.WallWidth);
-			Offset = new Vector2(-Assets.HalfWallWidth, -Assets.HalfWallWidth);
-		}
-
-		public Billboard(XElement xml) : this()
-		{
-			XML = xml;
-			if (XML?.Attribute("Name")?.Value is string name && !string.IsNullOrWhiteSpace(name))
-			{
-				Name = name;
-				CollisionShape.Name = "Collision " + name;
-			}
-			if (ushort.TryParse(XML?.Attribute("Page")?.Value, out ushort page))
-				Page = page;
-		}
-
 		public MeshInstance MeshInstance { get; set; } = null;
-
 		public ushort? Page
 		{
 			get => page;
@@ -69,21 +33,48 @@ namespace WOLF3D.WOLF3DGame.Action
 			}
 		}
 		private ushort? page = null;
-
-		public bool IsHit(Vector3 vector3) => IsHitLocal(ToLocal(vector3));
-		public bool IsHitLocal(Vector3 vector3) =>
-			Assets.VSwap.IsTransparent(
-				Page ?? 0,
-				(ushort)((vector3.x + Assets.HalfWallWidth) / Assets.WallWidth * Assets.VSwap.TileSqrt),
-				(ushort)(Assets.VSwap.TileSqrt - (vector3.y / Assets.WallHeight * Assets.VSwap.TileSqrt))
-				);
-
-		public override void _Process(float delta)
+		public XElement Serialize()
 		{
-			if (Visible)
-				Rotation = ActionRoom.BillboardRotation;
+			XElement e = new XElement(XName.Get("Billboard"));
+			e.SetAttributeValue(XName.Get("Page"), Page);
+			e.SetAttributeValue(XName.Get("X"), Transform.origin.x);
+			e.SetAttributeValue(XName.Get("Z"), Transform.origin.z);
+			e.SetAttributeValue(XName.Get("XML"), XML.ToString());
+			return e;
 		}
-
+		#endregion Data
+		#region Constructors
+		public Billboard()
+		{
+			Name = "Billboard";
+			AddChild(CollisionShape = new CollisionShape()
+			{
+				Shape = BillboardShape,
+				Transform = new Transform(Basis.Identity, new Vector3(0f, Assets.HalfWallHeight, -Assets.PixelWidth)),
+			});
+			CollisionShape.AddChild(MeshInstance = new MeshInstance()
+			{
+				Mesh = Assets.WallMesh,
+				Transform = new Transform(Basis.Identity, new Vector3(0f, 0f, Assets.PixelWidth)),
+				Visible = false,
+			});
+			Size = new Vector2(Assets.WallWidth, Assets.WallWidth);
+			Offset = new Vector2(-Assets.HalfWallWidth, -Assets.HalfWallWidth);
+		}
+		public Billboard(XElement xml) : this()
+		{
+			if (xml?.Attribute("Name")?.Value is string name && !string.IsNullOrWhiteSpace(name))
+			{
+				Name = name;
+				CollisionShape.Name = "Collision " + name;
+			}
+			if (ushort.TryParse(xml?.Attribute("Page")?.Value, out ushort page))
+				Page = page;
+			if (float.TryParse(xml?.Attribute("X")?.Value, out float x)
+				&& float.TryParse(xml?.Attribute("Z")?.Value, out float z))
+				Transform = new Transform(Transform.basis, new Vector3(x, 0f, z));
+			XML = xml.Attribute("XML")?.Value is string a ? XElement.Parse(a) : xml;
+		}
 		public static Billboard[] Billboards(GameMap map, byte difficulty = 4)
 		{
 			XElement objects = Assets.XML?.Element("VSwap")?.Element("Objects");
@@ -117,7 +108,23 @@ namespace WOLF3D.WOLF3DGame.Action
 					});
 			return billboards.ToArray();
 		}
-
+		#endregion Constructors
+		public bool IsHit(Vector3 vector3) => IsHitLocal(ToLocal(vector3));
+		public bool IsHitLocal(Vector3 vector3) =>
+			Assets.VSwap.IsTransparent(
+				Page ?? 0,
+				(ushort)((vector3.x + Assets.HalfWallWidth) / Assets.WallWidth * Assets.VSwap.TileSqrt),
+				(ushort)(Assets.VSwap.TileSqrt - (vector3.y / Assets.WallHeight * Assets.VSwap.TileSqrt))
+				);
+		public static readonly BoxShape BillboardShape = new BoxShape()
+		{
+			Extents = new Vector3(Assets.HalfWallWidth, Assets.HalfWallHeight, Assets.PixelWidth / 2f),
+		};
+		public override void _Process(float delta)
+		{
+			if (Visible)
+				Rotation = ActionRoom.BillboardRotation;
+		}
 		public int X => Assets.IntCoordinate(Transform.origin.x);
 		public int Z => Assets.IntCoordinate(Transform.origin.z);
 	}
---
+++ godot/WOLF3DGame/Action/ISavable.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace WOLF3D.WOLF3DGame.Action
+{
+	/// <summary>
+	/// For objects that can be serialized into the savegame files
+	/// </summary>
+	public interface ISavable
+	{
+		XElement Serialize();
+	}
+}
