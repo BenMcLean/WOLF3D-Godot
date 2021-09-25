7b62f495f2b35003d87b317668f3dc5141806ab2
Benjamin McLean <mclean.ben@gmail.com>
2021-09-24 19:42:11 -0500

    PushWall : ISavable

--- godot/WOLF3DGame/Action/FourWalls.cs
+++ godot/WOLF3DGame/Action/FourWalls.cs
@@ -1,19 +1,53 @@
 ﻿using Godot;
+using System.Xml.Linq;
 
 namespace WOLF3D.WOLF3DGame.Action
 {
-	public abstract class FourWalls : Pushable
+	public abstract class FourWalls : Pushable, ISavable
 	{
+		#region Data
 		public ushort X { get; set; } = 0;
 		public ushort Z { get; set; } = 0;
 		protected readonly MeshInstance[] Sides = new MeshInstance[4];
+		public ushort Wall
+		{
+			get => wall;
+			private set
+			{
+				wall = value;
+				Sides[DirectionIndex(Direction8.NORTH)].MaterialOverride = Assets.VSwapMaterials[wall];
+				Sides[DirectionIndex(Direction8.SOUTH)].MaterialOverride = Assets.VSwapMaterials[wall];
+			}
+		}
+		private ushort wall;
+		public ushort DarkSide
+		{
+			get => darkSide;
+			private set
+			{
+				darkSide = value;
+				Sides[DirectionIndex(Direction8.EAST)].MaterialOverride = Assets.VSwapMaterials[darkSide];
+				Sides[DirectionIndex(Direction8.WEST)].MaterialOverride = Assets.VSwapMaterials[darkSide];
+			}
+		}
+		private ushort darkSide;
+		public virtual XElement Save()
+		{
+			XElement e = new XElement(XName.Get(GetType().Name));
+			e.SetAttributeValue(XName.Get("X"), X);
+			e.SetAttributeValue(XName.Get("Z"), Z);
+			e.SetAttributeValue(XName.Get("Wall"), Wall);
+			e.SetAttributeValue(XName.Get("DarkSide"), DarkSide);
+			return e;
+		}
+		#endregion Data
 		/// <param name="cardinal">Cardinal direction</param>
 		/// <returns>Index corresponding to that direction in Sides</returns>
 		public static uint DirectionIndex(Direction8 cardinal) => cardinal.Value >> 1;
 		/// <param name="side">Array index from Sides</param>
 		/// <returns>Cardinal direction that side is facing</returns>
 		public static Direction8 Cardinal(uint side) => Direction8.From(side << 1);
-		public FourWalls(ushort wall, ushort darkSide)
+		protected FourWalls Set(ushort wall, ushort darkSide)
 		{
 			Name = "FourWalls";
 			CollisionShape shape;
@@ -26,6 +60,9 @@ namespace WOLF3D.WOLF3DGame.Action
 			AddChild(shape = Walls.BuildWall(wall, false, 1, 0)); // South
 			Sides[DirectionIndex(Direction8.SOUTH)] = (MeshInstance)shape.GetChild(0);
 			Size = new Vector2(Assets.WallWidth, Assets.WallWidth);
+			this.wall = wall;
+			this.darkSide = darkSide;
+			return this;
 		}
 	}
 }
--- godot/WOLF3DGame/Action/PushWall.cs
+++ godot/WOLF3DGame/Action/PushWall.cs
@@ -4,20 +4,54 @@ using System.Xml.Linq;
 
 namespace WOLF3D.WOLF3DGame.Action
 {
-	public class PushWall : FourWalls, ISpeaker
+	public class PushWall : FourWalls, ISpeaker, ISavable
 	{
 		public const float Seconds = 128f / 70f; // It takes 128 tics for a pushwall to fully open in Wolfenstein 3-D.
 		public const float HalfSeconds = Seconds / 2f;
-
+		#region Data
 		public XElement XML { get; set; } = null;
 		public int ArrayIndex { get; set; }
-		public PushWall(XElement xml) : base(
-			(ushort)(uint)xml.Attribute("Page"),
-			ushort.TryParse(xml.Attribute("DarkSide")?.Value, out ushort d) ? d : (ushort)(uint)xml.Attribute("Page")
-			)
+		public float? RepeatDigiSound = null;
+		public float SinceRepeatDigiSound = 0f;
+		public float Time = 0f;
+		public bool Halfway = false;
+		public bool Pushed
+		{
+			get => pushed;
+			set
+			{
+				pushed = value;
+				Play = Sound;
+			}
+		}
+		private bool pushed = false;
+		public Level Level { get; set; } = null;
+		public Direction8 Direction { get; set; }
+		public override XElement Save()
+		{
+			XElement e = base.Save(); // FourWalls
+			e.Name = XName.Get(GetType().Name);
+			e.SetAttributeValue(XName.Get("Time"), Time);
+			e.SetAttributeValue(XName.Get("Halfway"), Halfway);
+			e.SetAttributeValue(XName.Get("Pushed"), Pushed);
+			e.SetAttributeValue(XName.Get("Direction"), Direction.ToString());
+			if (RepeatDigiSound is float)
+			{
+				e.SetAttributeValue(XName.Get("RepeatDigiSound"), RepeatDigiSound);
+				e.SetAttributeValue(XName.Get("SinceRepeatDigiSound"), SinceRepeatDigiSound);
+			}
+			e.SetAttributeValue(XName.Get("XML"), XML.ToString());
+			return e;
+		}
+		#endregion Data
+		public PushWall(XElement xml)
 		{
 			Name = "Pushwall";
-			XML = xml;
+			XML = xml.Attribute("XML")?.Value is string a ? XElement.Parse(a) : xml;
+			Set(
+				(ushort)(uint)xml.Attribute("Page"),
+				ushort.TryParse(xml.Attribute("DarkSide")?.Value, out ushort d) ? d : (ushort)(uint)xml.Attribute("Page")
+				);
 			AddChild(Speaker = new AudioStreamPlayer3D()
 			{
 				Name = "Pushwall speaker",
@@ -25,7 +59,6 @@ namespace WOLF3D.WOLF3DGame.Action
 				Bus = "3D",
 			});
 		}
-
 		public override bool Push(Direction8 direction)
 		{
 			if (Pushed
@@ -37,9 +70,7 @@ namespace WOLF3D.WOLF3DGame.Action
 			Level.SetPushWallAt((ushort)(X + direction.X * 2), (ushort)(Z + direction.Z * 2), this);
 			return Pushed = true;
 		}
-
 		public bool PushWallOpen(int x, int z) => Level.Walls.IsNavigable(x, z) && !Level.IsPushWallAt((ushort)x, (ushort)z);
-
 		public override void _Process(float delta)
 		{
 			if (!Main.Room.Paused && Pushed == true && Time < Seconds)
@@ -90,24 +121,7 @@ namespace WOLF3D.WOLF3DGame.Action
 				}
 			}
 		}
-
-		public float? RepeatDigiSound = null;
-		public float SinceRepeatDigiSound = 0f;
-		public float Time = 0f;
-		public bool Halfway = false;
-		public bool Pushed
-		{
-			get => pushed;
-			set
-			{
-				pushed = value;
-				Play = Sound;
-			}
-		}
-		private bool pushed = false;
-
-		public Level Level { get; set; } = null;
-		public Direction8 Direction { get; set; }
+		#region ISpeaker
 		public AudioStreamPlayer3D Speaker { get; private set; }
 		public AudioStreamSample Play
 		{
@@ -120,5 +134,6 @@ namespace WOLF3D.WOLF3DGame.Action
 			}
 		}
 		public AudioStreamSample Sound { get; set; } = null;
+		#endregion ISpeaker
 	}
 }
