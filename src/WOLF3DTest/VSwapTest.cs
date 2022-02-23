18a2f756ea210aa92c7b4b7615121f83a0a701df
Benjamin McLean <mclean.ben@gmail.com>
2022-02-23 07:22:47 -0600

    made VSwap optional in atlas packing

--- src/WOLF3DTest/VSwapTest.cs
+++ src/WOLF3DTest/VSwapTest.cs
@@ -18,52 +18,54 @@ namespace WOLF3DTest
 			XElement xml = LoadXML(Folder);
 			VSwap vSwap = VSwap.Load(Folder, xml);
 			VgaGraph vgaGraph = VgaGraph.Load(Folder, xml);
-			PackingRectangle[] rectangles = PackingRectangles(vSwap, vgaGraph).ToArray();
+			PackingRectangle[] rectangles = PackingRectangles(vgaGraph, vSwap).ToArray();
 			RectanglePacker.Pack(rectangles, out PackingRectangle bounds, PackingHints.TryByBiggerSide);
 			int atlasSize = (int)TextureMethods.NextPowerOf2(bounds.BiggerSide);
 			byte[] bin = new byte[atlasSize * 4 * atlasSize];
 			foreach (PackingRectangle rectangle in rectangles)
-				if (TryTextureFromId(rectangle.Id, out byte[] texture, out int width, out int height, vSwap, vgaGraph))
+				if (TryTextureFromId(rectangle.Id, out byte[] texture, out int width, out int height, vgaGraph, vSwap))
 					bin.DrawInsert((int)rectangle.X + 1, (int)rectangle.Y + 1, texture, width, atlasSize)
 						.DrawPadding((int)rectangle.X + 1, (int)rectangle.Y + 1, width, height, atlasSize);
 			Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(bin, atlasSize, atlasSize)
 				.SaveAsPng("output.png");
 		}
-		public IEnumerable<PackingRectangle> PackingRectangles(VSwap vSwap, VgaGraph? vgaGraph = null)
+		public IEnumerable<PackingRectangle> PackingRectangles(VgaGraph? vgaGraph = null, VSwap? vSwap = null)
 		{
-			int total = vSwap.SoundPage;
-			if (vgaGraph is VgaGraph v)
-				total += v.Pics.Length + v.Fonts.Select(f => f.Character.Length).Sum();
+			int total = (vSwap is VSwap vs ? vs.SoundPage : 0)
+				+ (vgaGraph is VgaGraph vg ? vg.Pics.Length + vg.Fonts.Select(f => f.Character.Length).Sum() : 0);
 			for (int i = 0; i < total; i++)
-				if (TryTextureFromId(i, out byte[] _, out int width, out int height, vSwap, vgaGraph))
+				if (TryTextureFromId(i, out byte[] _, out int width, out int height, vgaGraph, vSwap))
 					yield return new PackingRectangle(0, 0, (uint)width + 2u, (uint)height + 2u, i);
 		}
-		public bool TryTextureFromId(int id, out byte[] texture, out int width, out int height, VSwap vSwap, VgaGraph? vgaGraph = null)
+		public bool TryTextureFromId(int id, out byte[] texture, out int width, out int height, VgaGraph? vgaGraph = null, VSwap? vSwap = null)
 		{
 			texture = null;
 			width = height = 0;
-			if (id < vSwap.SoundPage)
+			if (vSwap is VSwap vs)
 			{
-				if (vSwap.Pages[id] == null)
-					return false;
-				texture = vSwap.Pages[id];
-				width = height = vSwap.TileSqrt;
-				return true;
+				if (id < vs.SoundPage)
+				{
+					if (vs.Pages[id] == null)
+						return false;
+					texture = vs.Pages[id];
+					width = height = vs.TileSqrt;
+					return true;
+				}
+				id -= vs.SoundPage;
 			}
-			if (!(vgaGraph is VgaGraph v))
+			if (!(vgaGraph is VgaGraph vg))
 				return false;
-			id -= vSwap.SoundPage;
-			if (id < v.Pics.Length)
+			if (id < vg.Pics.Length)
 			{
-				if (v.Pics[id] == null)
+				if (vg.Pics[id] == null)
 					return false;
-				texture = v.Pics[id];
-				width = v.Sizes[id][0];
-				height = v.Sizes[id][1];
+				texture = vg.Pics[id];
+				width = vg.Sizes[id][0];
+				height = vg.Sizes[id][1];
 				return true;
 			}
-			id -= v.Pics.Length;
-			foreach (VgaGraph.Font font in v.Fonts)
+			id -= vg.Pics.Length;
+			foreach (VgaGraph.Font font in vg.Fonts)
 				if (id < font.Character.Length)
 				{
 					if (font.Character[id] == null)
