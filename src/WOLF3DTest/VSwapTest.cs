50030cac5a8dc5fae2b670aef42ee785bb8d9b44
Benjamin McLean <mclean.ben@gmail.com>
2022-02-22 22:05:55 -0600

    Fixed texture packing

--- src/WOLF3DTest/VSwapTest.cs
+++ src/WOLF3DTest/VSwapTest.cs
@@ -23,27 +23,20 @@ namespace WOLF3DTest
 			int atlasSize = (int)TextureMethods.NextPowerOf2(bounds.BiggerSide);
 			byte[] bin = new byte[atlasSize * 4 * atlasSize];
 			foreach (PackingRectangle rectangle in rectangles)
-			{
 				if (TryTextureFromId(rectangle.Id, out byte[] texture, out int width, out int height, vSwap, vgaGraph))
 					bin.DrawInsert((int)rectangle.X + 1, (int)rectangle.Y + 1, texture, width, atlasSize)
 						.DrawPadding((int)rectangle.X + 1, (int)rectangle.Y + 1, width, height, atlasSize);
-			}
 			Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(bin, atlasSize, atlasSize)
 				.SaveAsPng("output.png");
 		}
 		public IEnumerable<PackingRectangle> PackingRectangles(VSwap vSwap, VgaGraph? vgaGraph = null)
 		{
-			int number = 0;
-			for (; number < vSwap.SoundPage; number++)
-				if (vSwap.Pages[number] != null)
-					yield return new PackingRectangle(0, 0, vSwap.TileSqrt + 2u, vSwap.TileSqrt + 2u, number);
-			if (!(vgaGraph is VgaGraph v))
-				yield break;
-			for (int i = 0; i < v.Sizes.Length; i++)
-				yield return new PackingRectangle(0, 0, v.Sizes[i][0] + 2u, v.Sizes[i][1] + 2u, ++number);
-			foreach (VgaGraph.Font font in v.Fonts)
-				for (int i = 0; i < font.Width.Length; i++)
-					yield return new PackingRectangle(0, 0, font.Width[i] + 2u, font.Height + 2u, ++number);
+			int total = vSwap.SoundPage;
+			if (vgaGraph is VgaGraph v)
+				total += v.Pics.Length + v.Fonts.Select(f => f.Character.Length).Sum();
+			for (int i = 0; i < total; i++)
+				if (TryTextureFromId(i, out byte[] _, out int width, out int height, vSwap, vgaGraph))
+					yield return new PackingRectangle(0, 0, (uint)width + 2u, (uint)height + 2u, i);
 		}
 		public bool TryTextureFromId(int id, out byte[] texture, out int width, out int height, VSwap vSwap, VgaGraph? vgaGraph = null)
 		{
@@ -52,6 +45,8 @@ namespace WOLF3DTest
 			height = 0;
 			if (id < vSwap.SoundPage)
 			{
+				if (vSwap.Pages[id] == null)
+					return false;
 				texture = vSwap.Pages[id];
 				width = vSwap.TileSqrt;
 				height = vSwap.TileSqrt;
@@ -62,6 +57,8 @@ namespace WOLF3DTest
 			id -= vSwap.SoundPage;
 			if (id < v.Pics.Length)
 			{
+				if (v.Pics[id] == null)
+					return false;
 				texture = v.Pics[id];
 				width = v.Sizes[id][0];
 				height = v.Sizes[id][1];
@@ -69,16 +66,17 @@ namespace WOLF3DTest
 			}
 			id -= v.Pics.Length;
 			foreach (VgaGraph.Font font in v.Fonts)
-			{
 				if (id < font.Character.Length)
 				{
+					if (font.Character[id] == null)
+						return false;
 					texture = font.Character[id];
 					width = font.Width[id];
 					height = font.Height;
 					return true;
 				}
-				id -= font.Character.Length;
-			}
+				else
+					id -= font.Character.Length;
 			return false;
 		}
 		public static XElement LoadXML(string folder, string file = "game.xml")
